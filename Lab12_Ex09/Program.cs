f533b7a267368b2f96bb571a8b5cc504e32c8ec0
siriwan2004 <puttharasusiriwan@gmail.com>
2025-03-04 03:47:06 +0700

    Submit

---
+++ Lab12_Ex09/Program.cs
@@ -0,0 +1,23 @@
+﻿DerivedClass dc = new DerivedClass();
+dc.Greeting();
+
+class BaseClass
+{
+    public string Hello = "Hello From BaseClass";
+    public void Greeting()
+    {
+        Console.WriteLine("This is method in BaseClass");
+        Console.WriteLine(Hello);
+    }
+
+}
+class DerivedClass : BaseClass
+{
+    public string Hello = "Hello From DerivedClass";
+    public void Greeting()
+    {
+        Console.WriteLine("This is method in DerivedClass");
+        Console.WriteLine(Hello);
+    }
+
+}
